97b684abe71239b58f224d5020a9aee408518803
madan <madan9>
2022-03-31 07:47:18 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,128 +1,25 @@
-import React from 'react';
-import 'antd/dist/antd.css';
-import LayoutDemo from './LayoutDemo';
-import GridDemo from './components/GridDemo';
-import ImageDemo from './ImageDemo';
-import TypographyDemo from './TypographyDemo';
-import SpaceDemo from './SpaceDemo';
-import MessageDemo from './MessageDemo';
-import DatePickerDemo from './DatePickerDemo';
-import FormsDemo from './FormsDemo';
-import RateDemo from './RateDemo';
-import ModalForm from './ModalForm';
-import BasicModalDemo from './BasicModalDemo';
-import NotificationDemo from './NotificationDemo';
-import LineCharts from './LineCharts';
-import TwoLineChart from './TwoLineChart';
-import BarChartDemo from './BarChartDemo';
-import Demo from './TwoLineChart';
-import CardDemo from './CardDemo';
-import ButtonExample from './ButtonExample';
-import FormDemo from './pages/FormDemo';
-import SpinDemo from './SpinDemo';
-import SpinExample from './SpinExample';
-import TablePaginationDemo from './TablePaginationDemo';
-import ReactGoogleMaps from './ReactGoogleMaps';
-import ReactPlayerDemo from './ReactPlayerDemo';
-import Index from './components/Navbar';
-import GridExample from './pages/GridExample';
-import MoreResponsive from './pages/MoreResponsive';
-import ResponsiveCard from './pages/ResponsiveCard';
-import ReactImageMagnifyDemo from './pages/ReactImageMagnifyDemo';
-import ReactResponsiveCarousel from './pages/ReactResponsiveCarousel';
-import ReactAnimatedSlider from './pages/ReactAnimatedSlider';
-import ReactApexChart from './pages/ReactApexChart';
-import ImageCarousel from './pages/ImageCarousel';
-import ReactImageMagnifiers from './pages/ReactImageMagnifiers';
-import NavigationDemo from './pages/NavigationDemo';
-import DataEntry from './pages/DataEntry';
-import Home from './components/Navbar/Home';
-import StyledComponentDemo from './components/Navbar/StyledComponentDemo';
-import UnidirectionalDataFlow from './components/Navbar/UnidirectionalDataFlow';
-import ControlledUncontrolled from './components/ControlledUncontrolled';
-import RefDemo from './components/RefDemo';
-import ContextDemo from './components/ContextDemo';
-import UseMemoDemo from './components/UseMemoDemo';
-import ButtonDemo from './components/ButtonDemo';
-import SimpleForm from './components/SimpleForm';
-import DividerExample from './components/DividerExample';
-import AntDesignComponents from './components/AntDesignComponents';
-import RegistrationForm from './components/RegistrationForm';
-import TableDemo from './components/TableDemo';
-import TreeDemo from './components/TreeDemo';
-import ListDemo from './components/ListDemo';
-import ReactResponsive from './components/ReactResponsive';
-import GroupBy from './components/GroupBy';
-// import LayoutDemo from "./pages/LayoutDemo";
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <>
-      {/* <LayoutDemo /> */}
-      {/* <GridDemo /> */}
-      {/* <ImageDemo /> */}
-      {/* <ButtonDemo /> */}
-      {/* <TypographyDemo /> */}
-      {/* <SpaceDemo /> */}
-      {/* <MessageDemo /> */}
-      {/* <DatePickerDemo /> */}
-      {/* <FormsDemo /> */}
-      {/* <RateDemo /> */}
-      {/* <ModalForm /> */}
-      {/* <BasicModalDemo /> */}
-      {/* <NotificationDemo /> */}
-      {/* <LineCharts /> */}
-      {/* <TwoLineChart /> */}
-      {/* <BarChartDemo /> */}
-      {/* <Demo /> */}
-      {/* <FormDemo /> */}
-      {/* <CardDemo />
-      <ButtonExample /> */}
-      {/* <SpinDemo /> */}
-      {/* <SpinExample /> */}
-      {/* <TablePaginationDemo /> */}
-      {/* <ReactGoogleMaps
-        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAJwbaI1_EHmQV4y3W_WO4Sn3b9yf78FuY&v=3.exp&libraries=geometry,drawing,places"
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `400px` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-      /> */}
-      <ReactPlayerDemo />
-      {/* <Index /> */}
-      {/* <GridExample /> */}
-      {/* <MoreResponsive /> */}
-      {/* <ResponsiveCard /> */}
-      {/* <ReactImageMagnifyDemo /> */}
-
-      {/* <ReactResponsiveCarousel /> */}
-      {/* <ReactAnimatedSlider /> */}
-      {/* <ReactApexChart /> */}
-      {/* <ImageCarousel /> */}
-      {/* <ReactImageMagnifiers /> */}
-      {/* <LayoutDemo /> */}
-      {/* <NavigationDemo /> */}
-      {/* <DataEntry /> */}
-      {/* <Home /> */}
-      {/* <StyledComponentDemo /> */}
-      {/* <UnidirectionalDataFlow /> */}
-      {/* <ControlledUncontrolled /> */}
-      {/* <RefDemo /> */}
-      {/* <ContextDemo /> */}
-      {/* <UseMemoDemo /> */}
-      {/* <FormDemo /> */}
-      {/* <ButtonDemo /> */}
-      {/* <SimpleForm /> */}
-      {/* <DividerExample /> */}
-      {/* <AntDesignComponents /> */}
-      {/* <GridDemo /> */}
-      {/* <RegistrationForm /> */}
-      {/* <TableDemo />
-      <TreeDemo /> */}
-      {/* <ListDemo /> */}
-      {/* <ReactResponsive /> */}
-      {/* <GroupBy /> */}
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
--- src/pages/ReactImageMaginfy.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ReactImageMaginfy = () => {
-  return <div></div>;
-};
-
-export default ReactImageMaginfy;
